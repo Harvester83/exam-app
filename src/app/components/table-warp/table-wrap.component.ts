a3ed2fe7fa9925b58d43c9445e1c67c90e98e045
Rashad <rashad.suleymanov83@gmail.com>
2024-07-09 00:20:36 +0400

    Add Sorting

--- src/app/components/table-warp/table-wrap.component.ts
+++ src/app/components/table-warp/table-wrap.component.ts
@@ -1,15 +1,31 @@
-import { AfterViewInit, Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { LiveAnnouncer } from "@angular/cdk/a11y";
 
 @Component({
   standalone: true,
   selector: 'app-table-wrap',
   templateUrl: './table-wrap.component.html',
-  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
 })
 export class TableWrapComponent implements AfterViewInit {
   @Input() displayedColumns!: string[];
@@ -17,10 +33,29 @@ export class TableWrapComponent implements AfterViewInit {
 
   @Output() remove = new EventEmitter<string>();
   @Output() update = new EventEmitter<any>();
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    console.log(1, sortState);
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
   isDateColumn(column: string): boolean {
